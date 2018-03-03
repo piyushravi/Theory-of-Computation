9d67bf53ef06ea35c071a44d76b5474a347491e2
rachitrawat <rachit.rc.rawat@gmail.com>
2018-03-03 16:24:52 +0530

    int dict

--- NFA-to-DFA.py
+++ NFA-to-DFA.py
@@ -19,22 +19,43 @@ class eNFA:
         pprint.pprint(self.transition_function)
 
 
+def replaceBrackets(str):
+    lst = []
+    for element in str:
+        if element.isdigit():
+            lst.append(int(element))
+
+    return set(lst)
+
+
 with open("input") as f:
     temp = f.readlines()
     # remove null character
 temp = [x.strip() for x in temp]
+new_lst = []
 
-states = temp[0].split()
+for idx, val in enumerate(temp):
+    temp[idx] = temp[idx].replace('q', '')
+    lst = temp[idx].split()
+    for idx, val in enumerate(lst):
+        if '{' in val:
+            lst[idx] = replaceBrackets(val)
+        if len(val) == 1:
+            lst[idx] = int(val)
+    new_lst.append(lst)
+
+temp = new_lst
+states = temp[0]
 alphabet = {0, 1, 'e'}
-start_state = set(temp[1].split())
-accept_states = set(temp[2].split())
+start_state = set(temp[1])
+accept_states = set(temp[2])
 
 tf = {}
 for i in range(3, len(states) + 3):
     temp_dict = {}
     for idx, val in enumerate(alphabet):
-        temp_dict[val] = temp[i].split()[idx]
-    tf[states[i - 3]] = temp_dict
+        temp_dict[val] = temp[i][idx]
+    tf[i - 3] = temp_dict
 
 E1 = eNFA(states, alphabet, tf, start_state, accept_states)
 E1.display_details()
