a06316d9a85712b9de5812e18640636674a53081
rachitrawat <rachit.rc.rawat@gmail.com>
2018-03-03 13:07:53 +0530

    eNFA class

--- NFA-to-DFA.py
+++ NFA-to-DFA.py
@@ -1,9 +1,40 @@
+import pprint
+
+
+class eNFA:
+    def __init__(self, states, alphabet, transition_function, start_state, accept_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transition_function = transition_function
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.current_state = start_state
+
+    def display_details(self):
+        print("Q: ", self.states)
+        print("Σ: ", self.alphabet)
+        print("q0: ", self.start_state)
+        print("F: ", self.accept_states)
+        print("δ: ")
+        pprint.pprint(self.transition_function)
+
+
 with open("input") as f:
     temp = f.readlines()
     # remove null character
 temp = [x.strip() for x in temp]
 
 states = set(temp[0].split())
-alphabet = {0, 1}
+alphabet = {0, 1, 'e'}
 start_state = set(temp[1].split())
-final_states = set(temp[2].split())
+accept_states = set(temp[2].split())
+
+tf = {}
+for i in range(3, len(states) + 3):
+    temp_dict = {}
+    for idx, val in enumerate(alphabet):
+        temp_dict[val] = temp[i].split()[idx]
+    tf[i - 3] = temp_dict
+
+E1 = eNFA(states, alphabet, tf, start_state, accept_states)
+E1.display_details()
