e52b7a8620354a8a6060525297efb5066a14ffb9
rachitrawat <rachit.rc.rawat@gmail.com>
2018-03-03 12:06:06 +0530

    set up start, final states

--- NFA-to-DFA.py
+++ NFA-to-DFA.py
@@ -1,1 +1,9 @@
-# test
+with open("input") as f:
+    temp = f.readlines()
+    # remove null character
+temp = [x.strip() for x in temp]
+
+states = set(temp[0].split())
+alphabet = {0, 1}
+start_state = set(temp[1].split())
+final_states = set(temp[2].split())
