548f991df231e81cde8188dd18ed9f44d2e16166
Piyush Ravi <piyush.ravi_ug19@ashoka.edu.in>
2018-03-04 06:06:50 +0530

    Added visualize function.

--- DFA.py
+++ DFA.py
@@ -1,7 +1,13 @@
+import os
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from cStringIO import StringIO
+
 class DFA:
     def __init__(self, Q, initialState, alpha, delta, F):
         self.setQ(Q)
-        self.initialState = initialState
+        self.Q[initialState].isInitial = True
         self.alpha = alpha
         self.Delta = delta
         self.setF(F)
@@ -14,6 +20,7 @@ class DFA:
     def setF(self, F):
         for x in F:
             self.Q[x].setFinal()
+
 
     def delta(self, state, alpha):
         return self.Delta[(state, alpha)]
@@ -50,8 +57,77 @@ class DFA:
         else:
             print('The input {} is not in the language.'.format(inp))
 
-    def union(self, DFA):
+    def visualize(self):
+        plt.clf()
+        G = nx.MultiDiGraph()
+        states = self.Q.keys()
+
+        pos_x , pos_y = 1, 0
+        pos = {}
+        node_labels = {}
+        edge_labels = {}
+        flag_x, flag_y = False, True
+        for x in states:
+            label = x
+            if self.Q[x].isInitial:
+                label += '\ninitial'
+                
+            if self.Q[x].isFinal:
+                label += '\nfinal'
+            
+            G.add_node(x, pos=(pos_x, pos_y), label = label)
+                
+            
+
+            pos[x] = (pos_x, pos_y)
+            if flag_x:
+                pos_x += 1
+                flag_x = False
+            else:
+                flag_x = True
+
+            if flag_y:
+                pos_y += 1
+                flag_y = False
+            else:
+                flag_y = True
+
+            node_labels[x] = x
+
+            for a in self.alpha:
+                y = self.delta(x, a)
+                G.add_edge(x, y, label = a)
+
+                edge_labels[(x, y)] = a
+
+        # nx.drawing.nx_pydot.write_dot(G,'multi.dot')
+        D = nx.drawing.nx_pydot.to_pydot(G, 'multi.dot')
         
+#        for x in states:
+#            
+#
+#            for a in self.alpha:
+#                y = self.delta(x, a)
+#                G.add_edge(x, y)
+#
+#                edge_labels[(x, y)] = a
+            
+        png_str = D.create_png()
+        
+        sio = StringIO() # file-like string, appropriate for imread below
+        sio.write(png_str)
+        sio.seek(0)
+        plt.axis('off')
+        img = mpimg.imread(sio)
+        plt.imshow(img)
+        plt.savefig("toc_assgn3.4.png")
+        
+        # nx.draw_graphviz(G,prog='neato')
+        # plt.plot()
+
+    def union(self, DFA):
+        #to be implemented
+
         return 0
 
 
@@ -59,10 +135,10 @@ class DFA:
 
 
 class State:
-    def __init__(self, name, isFinal = False):
+    def __init__(self, name, isInitial = False, isFinal = False):
         self.name = name
-        self.isFinal = False
-
+        self.isFinal = isFinal
+        self.isInitial = isInitial
     def setFinal(self, isFinal = True):
         self.isFinal = isFinal
 
@@ -108,12 +184,13 @@ def getTransistionFunction(n):
 
 if __name__ == "__main__":
 
-    Q = getStates()
-    initialState = getInitialState()
-    alpha = getAlpha()
-    delta = getTransistionFunction()
-    F = getFinalStates()
+    Q = ['q0', 'q1', 'q2']
+    initialState = 'q0'
+    alpha = '01'
+    delta = [[1, 0], [0, 2], [2, 1]]
+    F = ['q1']
 
     firstDFA = DFA(Q, initialState, alpha, matrixToDict(Q, alpha, delta), F)
-    inp = input("Please enter the input for the DFA:")
-    firstDFA.runDFA(inp)
+    # inp = input("Please enter the input for the DFA:")
+    # firstDFA.runDFA(inp)
+    firstDFA.visualize()
